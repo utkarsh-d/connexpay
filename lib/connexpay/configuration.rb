e1c33a3607abdd392b7ea4c306fc64165f93f060
buoyant <isandeepthota@gmail.com>
2019-05-22 20:14:36 +0530

    code cleanup

--- lib/connexpay/configuration.rb
+++ lib/connexpay/configuration.rb
@@ -1,16 +1,13 @@
 module Connexpay
   class Configuration
-    attr_accessor :username, :password, :grant_type #, :merchant_id, :device_id, :token_expire_hours
+    attr_accessor :username, :password, :grant_type, :merchant_id, :device_id
 
     def initialize
-      @username   = 'username'
-      @password   = 'password'
-      @grant_type = 'grant_type'
-      # @merchant_id: 3fdf172b-f5d4-49d4-9fa5-41d8c1c79bd4
-      # @device_id: 58a2abe3-133e-44fc-8a77-16f2d10cf9
-      # @token_expire_hours: 24
-      # @username: 'srai'
-      # @password: 'Aa186a8c69a8f'
+      @username    = 'username'
+      @password    = 'password'
+      @grant_type  = 'grant_type'
+      @merchant_id = 'merchant_id'
+      @device_id   = 'device_id'
     end
   end
 end
